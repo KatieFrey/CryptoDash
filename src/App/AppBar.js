eea23a484bcc29cfc8395300fbe7e345c47fcab7
Katharine Francis <katie-francis@hotmail.com>
2019-04-29 22:41:01 -0400

    Updated AppBar with Control Button styles

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,44 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { blue } from "ansi-colors";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 
 const Bar = styled.div`
   display: grid;
+  margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
 `;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+//{name} - extract the names from the props
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+  );
+}
 
 export default () => {
   return (
     <Bar>
-      <div> CryptoDash </div>
+      <Logo> CryptoDash </Logo>
       <div />
-      <div> Dashboard </div>
-      <div> Settings </div>
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 };
