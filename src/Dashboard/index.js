eba6a0aea2fd1d30edb881250dd3b4a299c923c3
Katharine Francis <katie-francis@hotmail.com>
2019-05-02 18:38:10 -0400

    Add CoinSpotLight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 import Page from "../Shared/Page";
 import PriceGrid from "./PriceGrid";
+import CoinSpotLight from "./CoinSpotLight";
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default () => {
   return (
     <Page name="dashboard">
       <PriceGrid />
+      <ChartGrid>
+        <CoinSpotLight />
+        <div>Chart goes here</div>
+      </ChartGrid>
     </Page>
   );
 };
